853033dbfdebdca067a5aacb752337681e93b6fd
Amit Wani <mtwn105@gmail.com>
2022-08-26 18:55:34 +0530

    Added merchant related components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/merchant/login/login.component';
+import { RegisterComponent } from './components/merchant/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InventoryComponent } from './components/merchant/inventory/inventory.component';
+import { AddEditProductComponent } from './components/merchant/inventory/add-edit-product/add-edit-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    InventoryComponent,
+    AddEditProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
